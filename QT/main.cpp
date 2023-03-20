4e6f2896e8088f1e8672c6fd8de8f07b29ab2c9a
Cvazy <player7712@mail.ru>
2023-03-20 21:35:17 +0300

    Server :*

---
+++ QT/main.cpp
@@ -0,0 +1,12 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include "mytcpserver.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    MyTcpServer server;
+
+    return a.exec();
+}
---
+++ QT/mytcpserver.h
@@ -0,0 +1,24 @@
+#ifndef QT_MYTCPSERVER_H
+#define QT_MYTCPSERVER_H
+
+#include <QObject>
+#include <QTcpServer>
+#include <QTcpSocket>
+
+class MyTcpServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MyTcpServer(QObject *parent = 0);
+
+public slots:
+            void slotNewConnection();
+    void slotServerRead();
+    void slotClientDisconnected();
+
+private:
+    QTcpServer * mTcpServer;
+    QTcpSocket * mTcpSocket;
+};
+
+#endif
