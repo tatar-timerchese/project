03fabcbcab83f8485429c6a76bdf79a8e9a432ff
Cvazy <113592852+Cvazy@users.noreply.github.com>
2023-04-04 03:01:50 +0300

    Update mytcpserver.cpp

--- QT/mytcpserver.cpp
+++ QT/mytcpserver.cpp
@@ -1,41 +1,59 @@
 #include "mytcpserver.h"
+#include "Func_server.h"
 #include <QDebug>
 #include <QCoreApplication>
 
-MyTcpServer::MyTcpServer(QObject *parent) : QObject(parent)
-{
-    mTcpServer = new QTcpServer(this);
-
-    connect(mTcpServer, &QTcpServer::newConnection, this, &MyTcpServer::slotNewConnection);
-
-    if(!mTcpServer->listen(QHostAddress::Any, 6000)){
-        qDebug() << "server is not started";
-    } else {
-        qDebug() << "server is started";
-    }
-}
-
-void MyTcpServer::slotNewConnection()
-{
-    mTcpSocket = mTcpServer->nextPendingConnection();
-
-    mTcpSocket->write("Hello, World!!! I am echo server!\r\n");
-
-    connect(mTcpSocket, &QTcpSocket::readyRead, this, &MyTcpServer::slotServerRead);
-    connect(mTcpSocket, &QTcpSocket::disconnected, this, &MyTcpServer::slotClientDisconnected);
-}
-
-void MyTcpServer::slotServerRead()
-{
-    while(mTcpSocket->bytesAvailable()>0)
+MyTcpServer::~MyTcpServer()
+{}
+MyTcpServer::MyTcpServer(){
+    if(this->listen(QHostAddress::Any,33333))//Задаём адреса и порт подключения к серверу
     {
-        QByteArray array = mTcpSocket->readAll();
-
-        mTcpSocket->write(array);
+        qDebug() << "start";
+    }
+    else    {
+        qDebug() << "error";
     }
 }
 
-void MyTcpServer::slotClientDisconnected()
-{
-    mTcpSocket->close();
+void MyTcpServer::incomingConnection(qintptr socketDescriptor){
+    socket=new QTcpSocket;
+    socket->setSocketDescriptor(socketDescriptor);
+    connect(socket, &QTcpSocket::readyRead,this, &MyTcpServer::slotReadyRead);
+    connect(socket,&QTcpSocket::disconnected,socket,&QTcpSocket::deleteLater);
+
+    sockets.push_back(socket);
+    qDebug()<<"Client connected" <<socketDescriptor;
+}
+
+void MyTcpServer::slotReadyRead(){
+    socket=(QTcpSocket*)sender();
+    QDataStream in(socket);
+
+    if(in.status()==QDataStream::Ok){
+        qDebug() <<"Ready to read.";
+        QByteArray requestData="";
+        QString request = "";
+        while(socket->bytesAvailable()>0){
+        requestData = socket->readAll();;
+        request += QString::fromUtf8(requestData);
+        qDebug() <<"client said "<<request;
+        }
+        Parsing(requestData);
+    }
+    else{
+        qDebug() << "DataStream error";
+    }
+}
+void MyTcpServer::SendToClient(QString request){
+    qDebug() <<"Sending... "<<request;
+    data.clear();
+
+    if (request=="who\r\n") {
+        socket->write("cares\n");
+    }
+    
+    else {
+    QByteArray responseData= request.toUtf8();
+    socket->write("You said: " +responseData);}
+    socket->flush();
 }
